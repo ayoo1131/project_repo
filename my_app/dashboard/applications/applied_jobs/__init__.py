a88e5370fb1a1ff2931cc584c9cac0869ee26c72
Andrew <ayoo1131@ip-172-31-9-9.us-east-2.compute.internal>
2025-04-30 17:01:43 +0000

    Updated css styling

---
+++ my_app/dashboard/applications/applied_jobs/__init__.py
@@ -0,0 +1,16 @@
+#__init__.py
+
+from flask import Blueprint
+import os
+
+applied_jobs_blueprint = Blueprint(
+            'applied_jobs',
+            __name__,
+            static_folder='build/static', 
+            template_folder='build'
+        )
+
+#Import routes after Blueprint is created
+from . import routes
+
+
--- my_app/dashboard/applications/applied_jobs/routes.py
+++ my_app/dashboard/applications/applied_jobs/routes.py
@@ -1,7 +1,12 @@
 #routes.py
 
 from flask import BluePrint, send_from_directory, render_template
-import os
+from . import applied_jobs_blueprint
 
-@applied_jobs.route('/')
-def s
+@applied_jobs.route('/' methods=['GET', 'POST'])
+def applied_jobs_home():
+    if request.method == 'GET':
+        return render_template('applied_jobs.html')
+
+
+    return render_template('applied_jobs.html')
